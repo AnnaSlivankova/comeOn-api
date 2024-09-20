c3cf18cb063da02cbc64120010d2ddfbb8aef6fa
Hanna A. Slivankova <annslivankova@gmail.com>
2024-09-20 17:06:15 +0300

    add: add PORT

--- src/settings/app.settings.ts
+++ src/settings/app.settings.ts
@@ -8,7 +8,7 @@ export enum PATH {
 }
 
 export const CONFIG = {
-  PORT: process.env.PORT as string,
+  PORT: process.env.PORT as string || 3000,
   DB_URL: process.env.DB_URL as string,
   DB_NAME: process.env.DB_NAME as string,
   MONGO_URL: process.env.MONGO_URL as string,

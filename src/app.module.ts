26a5a7871e6003a4f5f50ca2ed92fc9b8b2f8c4b
Hanna A. Slivankova <annslivankova@gmail.com>
2024-09-20 16:55:31 +0300

    add: add config for deploy on Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { GameModule } from './features/game/game.module';
 import { CONFIG } from './settings/app.settings';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`${CONFIG.DB_URL}/${CONFIG.DB_NAME}`),
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot(`${CONFIG.DB_URL}/${CONFIG.DB_NAME}`),
+    MongooseModule.forRoot(`${CONFIG.MONGO_URL}`),
     ThrottlerModule.forRoot([
       {
         ttl: 10000,
@@ -19,5 +22,6 @@ import { MongooseModule } from '@nestjs/mongoose';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
 
--- src/settings/app.settings.ts
+++ src/settings/app.settings.ts
@@ -11,4 +11,5 @@ export const CONFIG = {
   PORT: process.env.PORT as string,
   DB_URL: process.env.DB_URL as string,
   DB_NAME: process.env.DB_NAME as string,
+  MONGO_URL: process.env.MONGO_URL as string,
 };
